5d6da3a308d74dd383f69714c42644e67e82781f
rajeshg007 <rajeshg1992@gmail.com>
2016-10-17 21:46:31 +0530

    files

--- Plot4.R
+++ Plot4.R
@@ -11,6 +11,9 @@ myData <- read.csv.sql(dataFile,mySql,header = TRUE,sep = ";")
 # Extract data for Global Active Power 
 globalActivePower <- as.numeric(myData$Global_active_power)
 
+# Extract data for global reactive power
+globalReactivePower <- as.numeric(myData$Global_reactive_power)
+
 # Extract datetime values
 datetime <- strptime(paste(myData$Date, myData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
 
@@ -22,7 +25,7 @@ subMetering2 <- as.numeric(myData$Sub_metering_2)
 subMetering3 <- as.numeric(myData$Sub_metering_3)
 
 # Extract Voltage
-voltage <- as.numeric(subSetData$Voltage)
+voltage <- as.numeric(myData$Voltage)
 
 #set the parameters for the image
 png("plot4.png", width=480, height=480)
@@ -30,10 +33,13 @@ png("plot4.png", width=480, height=480)
 # set the image parameters
 par(mfrow = c(2,2)) 
 
+#first Plot same as Plot1.png
 plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power", cex=0.2)
 
+#second Plot for voltage
 plot(datetime, voltage, type="l", xlab="datetime", ylab="Voltage")
 
+# 3rd plot same as Plot3.png
 plot(datetime, subMetering1, type="l", ylab="Energy Submetering", xlab="")
 
 lines(datetime, subMetering2, type="l", col="red")
@@ -42,6 +48,7 @@ lines(datetime, subMetering3, type="l", col="blue")
 
 legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=, lwd=2.5, col=c("black", "red", "blue"), bty="o")
 
+# 4th plot for reactive power
 plot(datetime, globalReactivePower, type="l", xlab="datetime", ylab="Global_reactive_power")
 
 dev.off()
